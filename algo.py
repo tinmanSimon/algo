143317efd7c5f4a8ad6835340ba8181d24165e8f
tinmanSimon <wudisas@gmail.com>
2022-01-22 09:56:23 -0800

    added myCountingSort

--- algo.py
+++ algo.py
@@ -24,7 +24,7 @@ class myHeap:
     def upwardHeapify(self, A, i):
         curI = i
         while(True):
-            parent = ((curI + 1) >> 1) - 1
+            parent = (curI >> 1) + 1
             if(parent >= 0 and self.mycmp(A[curI], A[parent])): 
                 A[parent], A[curI] = A[curI], A[parent]
                 curI = parent
@@ -53,20 +53,52 @@ class myHeap:
         res = A.pop()
         self.downwardHeapify(A, 0, len(A))
         return res
-        
+
+    def top(self):
+        if(self.A): return self.A[0]
+
+    def size(self):
+        return len(self.A)
+
+# maxVal is the upperbound of values in A.
+# lowerBound is default to be 0
+def myCountingSort(A, upperObject, lowerObject = 0, myhash = lambda a: a):
+    upperBound, lowerBound = myhash(upperObject), myhash(lowerObject)
+    if(upperBound < lowerBound): return []
+    res = [0 for i in A]
+    track = [0 for i in range(upperBound - lowerBound + 1)]
+    for val in A:
+        track[myhash(val) - lowerBound] += 1
+    for i in range(1, len(track)):
+        track[i] += track[i - 1]
+    for i in range(len(A) - 1, -1, -1):
+        trackIndex = myhash(A[i]) - lowerBound
+        track[trackIndex] -= 1
+        res[track[trackIndex]] = A[i]
+    return res
 
 
 
-#heapSort(A)
-#print(A)
-#heapSort(A, False)
-#print(A)
-h = myHeap([1,10,14,2,17,13,6,5,7,12])
-print(h)
-print(h.sort())
-print(h.pop(), h)
-h.add(30)
-print(h)
-for i in range(30):
-    k = h.pop()
-    print(k, h)
+
+A = [-5,1,10,14,2,17,13,6,5,7,12,-3,-2,-1,-2,-3,0,3,2,-4,9,10,8]
+print(myCountingSort(A, 20, -5))
+
+B = [(99, i) for i in A]
+mycmp = lambda a: a[1]
+print(myCountingSort(B, (99, 20), (99, -5), mycmp))
+
+C = [(99, 0) for i in A]
+print(myCountingSort(C, (99, 0), (99, 0), mycmp))
+
+C = [(99, -4) for i in A]
+print(myCountingSort(C, (99, -4), (99, -4), mycmp))
+
+C = [(99, 1) for i in A]
+print(myCountingSort(C, (99, 1), (99, 1), mycmp))
+
+C = [-3 for i in A]
+print(myCountingSort(C, -3, -3))
+
+print(myCountingSort([], -3, -3))
+
+print(myCountingSort([-3], -3, -3))
